26c9afa481a3e27c0183b71076596193e9c9502a
DAVID <67152500+owolagbadavid@users.noreply.github.com>
2022-09-18 17:04:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -13,8 +13,7 @@ const helmet = require('helmet');
 
 var app = express();
 const mongoose = require('mongoose');
-const dev_db_url ='mongodb+srv://oreosinnit:ddaviddo12@cluster0.z8gyhrr.mongodb.net/local_library?retryWrites=true&w=majority'
-const mongoDB = process.env.MONGODB_URI || dev_db_url;
+const mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
